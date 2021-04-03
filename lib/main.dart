15ef1632282812376ee234442a8e77d6ecc2ed05
yodyindra <indrayody04@gmail.com>
2021-04-03 20:40:57 +0800

    menempatkan  teks hello world berada di tengah

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('hello world'));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first App'),
+        ),
+        body: Center(child: Text('Hello world')),
+      ),
+    );
   }
 }
