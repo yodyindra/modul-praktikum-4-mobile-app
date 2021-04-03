b8bbfc812f78aa5a7fe84dee3e13cf004c6eae51
yodyindra <indrayody04@gmail.com>
2021-04-03 20:47:07 +0800

    penggunaan function “jawab” pada “onPressed”

--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,23 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('My first App'),
-          ),
-          body: Column(children: <Widget>[
-            Text('Pertanyaan!'),
-            RaisedButton(
-              child: Text('Jawaban 1'),
-              onPressed: null,
-            ),
-            RaisedButton(
-              child: Text('Jawaban 2'),
-              onPressed: null,
-            ),
-            RaisedButton(
-              child: Text('Jawaban 3'),
-              onPressed: null,
-            )
-          ])),
-    );
-  }
+  jawab() {
+   print('jawaban dipilih!')
+ }
+ 
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     home: Scaffold(
+       appBar: AppBar(
+         title: Text('My first App'),
+       ),
+       body: Column(children: <Widget>[
+         Text('Pertanyaan!'),
+         RaisedButton(child: Text('Jawaban 1'), onPressed: jawab,),
+         RaisedButton(child: Text('Jawaban 2'), onPressed: jawab,),
+         RaisedButton(child: Text('Jawaban 3'), onPressed: jawab,)
+       ])
+     ),
+   );
+ }
 }
