49f52673c1b5fab3c3710f253ff5b07e10a6d420
yodyindra <indrayody04@gmail.com>
2021-04-03 20:42:49 +0800

    menggunakan invisible widget Column

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My first App'),
-        ),
-        body: Center(child: Text('Hello world')),
-      ),
+          appBar: AppBar(
+            title: Text('My first App'),
+          ),
+          body: Column(children: <Widget>[
+            Text('Pertanyaan!'),
+            RaisedButton(
+              child: Text('Jawaban 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Jawaban 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Jawaban 3'),
+              onPressed: null,
+            )
+          ])),
     );
   }
 }
