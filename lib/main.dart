c902e977b8f97b9e0b5e61ea5fbcac2d7411dbb1
yodyindra <indrayody04@gmail.com>
2021-04-03 21:00:45 +0800

    penggunaan anonymous function

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,32 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   jawab() {
-   print('jawaban dipilih!')
- }
- 
- Widget build(BuildContext context) {
-   return MaterialApp(
-     home: Scaffold(
-       appBar: AppBar(
-         title: Text('My first App'),
-       ),
-       body: Column(children: <Widget>[
-         Text('Pertanyaan!'),
-         RaisedButton(child: Text('Jawaban 1'), onPressed: jawab,),
-         RaisedButton(child: Text('Jawaban 2'), onPressed: jawab,),
-         RaisedButton(child: Text('Jawaban 3'), onPressed: jawab,)
-       ])
-     ),
-   );
- }
+    print('jawaban dipilih!');
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('My first App'),
+          ),
+          body: Column(children: <Widget>[
+            Text('Pertanyaan!'),
+            RaisedButton(
+              child: Text('Jawaban 1'),
+              onPressed: () => print('jawaban dipilih!'),
+            ),
+            RaisedButton(
+              child: Text('Jawaban 2'),
+              onPressed: () {
+                print('jawaban dipilih!');
+              },
+            ),
+            RaisedButton(
+              child: Text('Jawaban 3'),
+              onPressed: () => print('jawaban dipilih!'),
+            )
+          ])),
+    );
+  }
 }
